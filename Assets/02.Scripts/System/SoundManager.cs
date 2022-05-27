901f40b18dd9f98dc7fc4c93292cb18a10d7073b
khh7052 <59150989+khh7052@users.noreply.github.com>
2022-05-27 22:47:53 +0900

    2022-05-27
    폰트추가 (주아체)

---
+++ Assets/02.Scripts/System/SoundManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource speaker;
+
+    public void PlaySound()
+    {
+        speaker.Play();
+    }
+
+}
