1e813e0e89862c748f85b46db07724984d173ffb
khh7052 <59150989+khh7052@users.noreply.github.com>
2022-11-22 18:55:57 +0900

    11.22
    시작 전 튜토리얼용 Touch 이미지, 애니메이션 추가

--- Assets/02.Scripts/System/ReadyLine.cs
+++ Assets/02.Scripts/System/ReadyLine.cs
@@ -7,10 +7,17 @@ public class ReadyLine : MonoBehaviour
 {
     [SerializeField] private Line line;
     [SerializeField] private float dashOffsetSpeed = 2;
+    [SerializeField] private GameObject touchImage;
 
     void Update()
     {
         DashOffsetUpdate();
+
+        if(Input.touchCount > 0)
+        {
+            touchImage.SetActive(false);
+            gameObject.SetActive(false);
+        }
     }
 
     void DashOffsetUpdate()
